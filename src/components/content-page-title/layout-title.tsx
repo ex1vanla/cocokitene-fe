bdc07a46e404ab1f8f48ca104705392a73f74ee1
TTM-THUYNT\ttm-dev <huynt@trithucmoi.co>
2023-10-29 09:54:16 +0700

    refactor: Fix title layout sticky

--- src/components/content-page-title/layout-title.tsx
+++ src/components/content-page-title/layout-title.tsx
@@ -10,7 +10,7 @@ export interface ILayoutTitle {
 
 const LayoutTitle = ({ children }: ILayoutTitle) => {
     return (
-        <div className="content-title flex items-center justify-between bg-white px-6 py-4">
+        <div className="content-title flex items-center justify-between bg-white px-6 py-4 sticky top-12 z-10">
             {children}
         </div>
     )
