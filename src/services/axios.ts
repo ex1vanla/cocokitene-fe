379c21c1def464863202e9522245272c7cc217e4
TTM-THUYNT\ttm-dev <huynt@trithucmoi.co>
2023-12-13 09:36:49 +0700

    refactor: Configuring Environment Variables

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: process.env.API_ENDPOINT,
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
