5137e809ea242dfa803cab8ae84f8fea7ae7392a
TienNV <tiennv@trithucmoi.co>
2024-02-26 14:05:12 +0700

    feat: fix_SupAdminStatus

--- src/services/user-status.ts
+++ src/services/user-status.ts
@@ -1,5 +1,6 @@
 import { IGetAllDataRequest } from '@/services/request.type'
 import { get } from './fetcher'
+import { get as getSys } from './system-admin/fetcher-system'
 import { IGetAllDataReponse, IUserStatusResponse } from './response.type'
 
 const serviceUserStatus = {
@@ -13,6 +14,17 @@ const serviceUserStatus = {
 
         return response.data.items
     },
+    getAllUserStatusSysAdmin : async ({
+        page,
+        limit,
+    }: IGetAllDataRequest): Promise<IUserStatusResponse[]> => {
+        const payload = { page, limit }
+        const response: { data: IGetAllDataReponse<IUserStatusResponse> } =
+            await getSys('/system-admin/user-status', payload)
+
+        return response.data.items
+    },
+
 }
 
 export default serviceUserStatus
