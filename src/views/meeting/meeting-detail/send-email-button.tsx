fcfc9454835fcfb264e1dbec48517713ba018de8
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2024-03-07 16:28:09 +0700

    feat: add logic send email in meeting

--- src/views/meeting/meeting-detail/send-email-button.tsx
+++ src/views/meeting/meeting-detail/send-email-button.tsx
@@ -1,15 +1,56 @@
 import { MailOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
+import { Button, notification } from 'antd'
 import { useTranslations } from 'next-intl'
+import { useState } from 'react'
+import { FETCH_STATUS } from '@/constants/common'
+import serviceMeeting from '@/services/meeting'
+import { useParams } from 'next/navigation'
+import { AxiosError } from 'axios'
 
 const SendEmailButton = () => {
     const t = useTranslations()
+    const params = useParams()
+    const meetingId: number = Number(params.id)
+
+    console.log(typeof meetingId)
+    const [status, setStatus] = useState(FETCH_STATUS.IDLE)
+
+    const handleClick = async () => {
+        setStatus(FETCH_STATUS.LOADING)
+        try {
+            const response =
+                await serviceMeeting.sendMailInvitationShareholderMeeting(
+                    meetingId,
+                )
+            if (response) {
+                notification.success({
+                    message: t('CREATED'),
+                    description: t(
+                        'SEND_EMAIL_TO_SHAREHOLDER_MEETING_SUCCESSFULLY',
+                    ),
+                })
+                setStatus(FETCH_STATUS.SUCCESS)
+            }
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                notification.error({
+                    message: t('ERROR'),
+                    description: error.response?.data.info.message,
+                })
+            }
+            setStatus(FETCH_STATUS.ERROR)
+        }
+    }
+
     return (
         <Button
             icon={<MailOutlined />}
             type="primary"
             size="large"
-            onClick={() => {}}
+            loading={status === FETCH_STATUS.LOADING}
+            onClick={() => {
+                handleClick()
+            }}
         >
             {t('SEND_EMAIL_TO_SHAREHOLDERS')}
         </Button>
