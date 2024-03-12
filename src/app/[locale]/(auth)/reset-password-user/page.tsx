379ac97535683cdcb13cabee194fbff9ea489bfa
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2024-03-12 13:26:40 +0700

    feat: add creen reset password user

--- src/app/[locale]/(auth)/reset-password-user/page.tsx
+++ src/app/[locale]/(auth)/reset-password-user/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
-import ResetPasswordUser from '@/views/forgot-password-user/reset-password-user'
+
+import ResetPasswordUser from '@/views/reset-password-user'
 
 export default ResetPasswordUser
