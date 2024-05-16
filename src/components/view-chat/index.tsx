ed0e236b76bcb74230678d5f8bfd739c46c004c2
TienNV <tiennv@trithucmoi.co>
2024-05-16 15:47:33 +0700

    feat: update_UI_chatMtg

--- src/components/view-chat/index.tsx
+++ src/components/view-chat/index.tsx
@@ -1,80 +1,26 @@
 /* eslint-disable */
 
 import { MessageTwoTone, SendOutlined } from '@ant-design/icons'
-import { Modal, Select } from 'antd'
+import { Modal, Row, Select, Spin, message } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
-import { ChangeEvent, useMemo, useState } from 'react'
+import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 import {
     MessageChatItemFromYou,
     MessageChatItemToYou,
 } from './message-chat-item'
 import { useAuthLogin } from '@/stores/auth/hooks'
-
-const fakeDataChat: { from: string; to: string; message: string }[] = [
-    {
-        from: 'nva01@gmail.com',
-        to: 'tiennv@trithucmoi.co',
-        message: 'Test Chat Item',
-    },
-    {
-        from: 'nva01@gmail.com',
-        to: 'everyone',
-        message:
-            'Test Chat Item2 Test Chat Item2 Test Chat Item2 Test Chat Item2 Test Chat Item2',
-    },
-    {
-        from: 'nva01@gmail.com',
-        to: 'everyone',
-        message: 'Test Chat Item3',
-    },
-    {
-        from: 'nva02@gmail.com',
-        to: 'tiennv@trithucmoi.co',
-        message: 'Test Chat Item4',
-    },
-    {
-        from: 'nva02@gmail.com',
-        to: 'everyone',
-        message: 'Test Chat Item5',
-    },
-    {
-        from: 'nva02@gmail.com',
-        to: 'everyone',
-        message: 'Test Chat Item6',
-    },
-    {
-        from: 'nva01@gmail.com',
-        to: 'everyone',
-        message: 'Test Chat Item7',
-    },
-    {
-        from: 'nva01@gmail.com',
-        to: 'everyone',
-        message: 'Test Chat Item7',
-    },
-    {
-        from: 'nva01@gmail.com',
-        to: 'everyone',
-        message: 'Test Chat Item7',
-    },
-    {
-        from: 'nva02@gmail.com',
-        to: 'nva01@gmail.com',
-        message: 'Test Chat Item7',
-    },
-]
-
-interface IParticipantDetail1 {
-    userId: number
-    avatar?: string
-    userEmail: string
-    shareQuantity?: number | null
-}
+import { DataMessageChat } from '@/services/response.type'
+import { FETCH_STATUS } from '@/constants/common'
+import serviceChatMeeting from '@/services/chat-meeting'
 
 interface IParticipantDetail {
     roleMtgId: number
     roleMtgName: string
-    userParticipants: IParticipantDetail1[]
+    userParticipants: {
+        userId: number
+        avatar?: string
+        userEmail: string
+    }[]
 }
 
 interface IMeetingInfo {
@@ -90,11 +36,48 @@ interface IMeetingChat {
 const MeetingChat = ({ meetingInfo }: IMeetingChat) => {
     const { authState } = useAuthLogin()
 
+    const [dataChat, setDataChat] = useState<{
+        roomChat: number
+        messageChat: DataMessageChat[]
+    }>()
     const [chatModalOpen, setChatModalOpen] = useState<boolean>(false)
     const [valueMessage, setValueMessage] = useState<string>('')
     const [sendToUser, setSendToUser] = useState<number>(0)
+    const [initStatus, setInitStatus] = useState<FETCH_STATUS>(
+        FETCH_STATUS.IDLE,
+    )
 
-    const userJoinChat = useMemo(() => {
+    console.log('dataChat', dataChat)
+
+    useEffect(() => {
+        const fetchDataChat = async (meetingId: number) => {
+            setInitStatus(FETCH_STATUS.LOADING)
+            try {
+                const res =
+                    await serviceChatMeeting.getAllMessageChatByMeetingId(
+                        meetingId,
+                    )
+                if (res) {
+                    setDataChat({
+                        ...res,
+                        messageChat: res.messageChat.map((message) => ({
+                            ...message,
+                            receiverId: message.receiverId
+                                ? message.receiverId
+                                : 0,
+                        })),
+                    })
+                    setInitStatus(FETCH_STATUS.SUCCESS)
+                }
+            } catch (error) {}
+        }
+
+        if (meetingInfo.id) {
+            fetchDataChat(meetingInfo.id)
+        }
+    }, [meetingInfo.id])
+
+    const participantToSendMessage = useMemo(() => {
         const participant = meetingInfo.participants
             .map((participant) => participant.userParticipants)
             .flatMap((user) => user)
@@ -122,6 +105,28 @@ const MeetingChat = ({ meetingInfo }: IMeetingChat) => {
         ]
     }, [...meetingInfo.participants])
 
+    const participantJoinChat = useMemo((): { [key in number]: string } => {
+        const participant = meetingInfo.participants
+            .map((participant) => participant.userParticipants)
+            .flatMap((user) => user)
+
+        const cachedObject: {
+            [key in string]: string
+        } = {}
+        participant.forEach((obj) => {
+            if (!cachedObject[obj.userId]) {
+                cachedObject[obj.userId] = obj.userEmail
+            }
+        })
+
+        return {
+            0: 'EveryOne',
+            ...cachedObject,
+        }
+    }, [...meetingInfo.participants])
+
+    console.log('participantJoinChat: ', participantJoinChat)
+
     const toggleModelDetailResolution = () => {
         setChatModalOpen(!chatModalOpen)
     }
@@ -132,7 +137,7 @@ const MeetingChat = ({ meetingInfo }: IMeetingChat) => {
 
     //
     const choiceSendMessageTo = (e: string) => {
-        const idOfUserToSendMessage = userJoinChat.find(
+        const idOfUserToSendMessage = participantToSendMessage.find(
             (user) => user.userEmail.toLowerCase() == e.toLowerCase(),
         )
         if (idOfUserToSendMessage) {
@@ -162,95 +167,160 @@ const MeetingChat = ({ meetingInfo }: IMeetingChat) => {
                 footer={[]}
             >
                 <div className="flex h-[600px] flex-col gap-2">
-                    <div className="flex h-[5%] items-center justify-center gap-2">
-                        <div className="text-xl font-medium">
-                            Chat {meetingInfo.title}
-                        </div>
-                    </div>
-                    <div className="border-black-500 over h-[77%] overflow-y-auto border p-2">
-                        {fakeDataChat.map((message, index) => {
-                            if (
-                                message.from.toLowerCase() ==
-                                authState.userData?.email.toLowerCase()
-                            ) {
-                                return (
-                                    <MessageChatItemFromYou
-                                        key={index}
-                                        from={message.from}
-                                        to={message.to}
-                                        message={message.message}
-                                        messageInfoPrev={
-                                            fakeDataChat[index - 1]
-                                        }
-                                        setSentUserTo={choiceSendMessageTo}
-                                    />
-                                )
-                            }
-
-                            return (
-                                <MessageChatItemToYou
-                                    key={index}
-                                    from={message.from}
-                                    to={
-                                        message.to.toLowerCase() ==
-                                        authState.userData?.email.toLowerCase()
-                                            ? 'You'
-                                            : message.to
+                    {initStatus === FETCH_STATUS.LOADING ? (
+                        <Row
+                            align={'middle'}
+                            justify={'center'}
+                            style={{ height: '40vh' }}
+                        >
+                            <Spin tip="Loading..." />
+                        </Row>
+                    ) : (
+                        <>
+                            <div className="flex h-[5%] items-center justify-center gap-2">
+                                <div className="text-xl font-medium">
+                                    Chat {meetingInfo.title}
+                                </div>
+                            </div>
+                            <div className="border-black-500 over h-[77%] overflow-y-auto border p-2">
+                                {dataChat?.messageChat.map((message, index) => {
+                                    if (
+                                        message.senderId ==
+                                        authState.userData?.id
+                                    ) {
+                                        return (
+                                            <MessageChatItemFromYou
+                                                key={index}
+                                                from={
+                                                    participantJoinChat[
+                                                        message.senderId
+                                                    ]
+                                                }
+                                                to={
+                                                    participantJoinChat[
+                                                        message.receiverId
+                                                    ]
+                                                }
+                                                message={message.content}
+                                                messageInfoPrev={{
+                                                    from: participantJoinChat[
+                                                        dataChat.messageChat[
+                                                            index - 1
+                                                        ]?.senderId
+                                                    ],
+                                                    to: participantJoinChat[
+                                                        dataChat.messageChat[
+                                                            index - 1
+                                                        ]?.receiverId
+                                                    ],
+                                                }}
+                                                setSentUserTo={
+                                                    choiceSendMessageTo
+                                                }
+                                            />
+                                        )
                                     }
-                                    message={message.message}
-                                    messageInfoPrev={fakeDataChat[index - 1]}
-                                    setSentUserTo={choiceSendMessageTo}
-                                />
-                            )
-                        })}
-                    </div>
-                    <div className="flex h-[15%] gap-5">
-                        <div className="flex w-[95%] flex-col gap-2">
-                            <div>
-                                <span>To : </span>
-                                <Select
-                                    defaultValue={'0'}
-                                    value={String(sendToUser)}
-                                    showSearch
-                                    placeholder="Select a person"
-                                    optionFilterProp="children"
-                                    onChange={onChange}
-                                    // onSearch={onSearch}
-                                    filterOption={filterOption}
-                                    options={userJoinChat.map((user) => ({
-                                        value: user.userId + '',
-                                        label: user.userEmail,
-                                    }))}
-                                    size={'small'}
-                                    className="w-[50%] rounded-[7px]"
-                                />
-                            </div>
 
-                            <div className="flex w-full items-center gap-5">
-                                <TextArea
-                                    value={valueMessage}
-                                    onChange={(e) => pressMessageChat(e)}
-                                    placeholder="Please input message"
-                                    autoSize={{ minRows: 2, maxRows: 2 }}
-                                    onKeyUp={(e) => {
-                                        if (e.keyCode == 13 && !e.shiftKey) {
-                                            sendMessage()
-                                        }
-                                        if (e.keyCode == 13 && e.shiftKey) {
-                                            console.log('Shift + Enter Key!!!!')
-                                        }
-                                    }}
-                                />
-                                <SendOutlined
-                                    style={{
-                                        fontSize: '22px',
-                                        color: '#5151e5',
-                                    }}
-                                    onClick={sendMessage}
-                                />
+                                    return (
+                                        <MessageChatItemToYou
+                                            key={index}
+                                            from={
+                                                participantJoinChat[
+                                                    message.senderId
+                                                ]
+                                            }
+                                            to={
+                                                participantJoinChat[
+                                                    message.receiverId
+                                                ].toLowerCase() ==
+                                                authState.userData?.email.toLowerCase()
+                                                    ? 'You'
+                                                    : participantJoinChat[
+                                                          message.receiverId
+                                                      ]
+                                            }
+                                            message={message.content}
+                                            messageInfoPrev={{
+                                                from: participantJoinChat[
+                                                    dataChat.messageChat[
+                                                        index - 1
+                                                    ]?.senderId
+                                                ],
+                                                to: participantJoinChat[
+                                                    dataChat.messageChat[
+                                                        index - 1
+                                                    ]?.receiverId
+                                                ],
+                                            }}
+                                            setSentUserTo={choiceSendMessageTo}
+                                        />
+                                    )
+                                })}
                             </div>
-                        </div>
-                    </div>
+                            <div className="flex h-[15%] gap-5">
+                                <div className="flex w-[95%] flex-col gap-2">
+                                    <div>
+                                        <span>To : </span>
+                                        <Select
+                                            defaultValue={'0'}
+                                            value={String(sendToUser)}
+                                            showSearch
+                                            placeholder="Select a person"
+                                            optionFilterProp="children"
+                                            onChange={onChange}
+                                            // onSearch={onSearch}
+                                            filterOption={filterOption}
+                                            options={participantToSendMessage.map(
+                                                (user) => ({
+                                                    value: user.userId + '',
+                                                    label: user.userEmail,
+                                                }),
+                                            )}
+                                            size={'small'}
+                                            className="w-[50%] rounded-[7px]"
+                                        />
+                                    </div>
+
+                                    <div className="flex w-full items-center gap-5">
+                                        <TextArea
+                                            value={valueMessage}
+                                            onChange={(e) =>
+                                                pressMessageChat(e)
+                                            }
+                                            placeholder="Please input message"
+                                            autoSize={{
+                                                minRows: 2,
+                                                maxRows: 2,
+                                            }}
+                                            onKeyUp={(e) => {
+                                                if (
+                                                    e.keyCode == 13 &&
+                                                    !e.shiftKey
+                                                ) {
+                                                    sendMessage()
+                                                }
+                                                if (
+                                                    e.keyCode == 13 &&
+                                                    e.shiftKey
+                                                ) {
+                                                    console.log(
+                                                        'Shift + Enter Key!!!!',
+                                                    )
+                                                }
+                                            }}
+                                        />
+                                        <SendOutlined
+                                            style={{
+                                                fontSize: '22px',
+                                                color: '#5151e5',
+                                            }}
+                                            onClick={sendMessage}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </div>
             </Modal>
 
