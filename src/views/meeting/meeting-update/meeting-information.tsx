115331f370390d1aed0efc32fe8753916a1884eb
TienNV <tiennv@trithucmoi.co>
2024-07-10 14:35:36 +0700

    refactor: [12416]-edit_database

--- src/views/meeting/meeting-update/meeting-information.tsx
+++ src/views/meeting/meeting-update/meeting-information.tsx
@@ -285,7 +285,7 @@ const MeetingInformation = () => {
                     <Form layout="vertical">
                         <Form.Item
                             name="meetingLink"
-                            label={t('MEETING_LINK')}
+                            label={t('MEETING_LINKS')}
                             className="mb-0"
                             rules={[
                                 {
